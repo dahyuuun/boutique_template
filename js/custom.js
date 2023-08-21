51a1c0e16245b1a233cfc265480d86f6314f427e
dahyuuun <dohyuuun@gmail.com>
2023-08-21 12:48:20 +0900

    category section responsive

--- js/custom.js
+++ js/custom.js
@@ -46,3 +46,39 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+//mobile header toggle active
+//1. 아이콘 클릭 -> 2. 아이콘에 'on' 클래스 추가(토글) -> 3.네비게이션 높이  저장 -> 4. 'on' 클래스가 있을 때 네비게이션 활성화 -> 5. 'on' 클래스가 없을 때 네비게이션 비 활성화
+
+const menuIcon = document.querySelector('.menu-icon'); //메뉴 아이콘 요소 저장
+const navi = document.querySelector('.navi'); //네비게이션 박스 요소 저장
+
+/* 함수는 3가지 종류가 있다. 화살표 함수,  추가되는 이벤트는 click 기능은 function*/
+menuIcon.addEventListener('click', function () {
+  this.classList.toggle('on'); // menu-icon 클릭할 때마다 on 클래스 추가 및 제거
+
+  const navHeight = navi.scrollHeight; //navi 박스의 자식요소를 포함한 높이
+  console.log(navHeight);
+
+  if (this.classList.contains('on')) {
+    // menu-icon에 on 클래스가 있다면,
+    // console.log(true);
+    navi.style.height = navHeight + 'px';
+  } else {
+    // menu-icon에 on 클래스가 없다면  (false를 콘솔에 출력함),
+    // console.log(false);
+    navi.style.height = 0;
+  }
+});
+
+// If PC size browser,  navigation height to normal
+window.addEventListener('resize', function () {
+  const winWidth = window.outerWidth; //윈도우 화면 가로 사이즈 저장
+
+  if (winWidth > 980) {
+    menuIcon.classList.remove('on');
+    navi.style.height = 'auto';
+  } else {
+    navi.style.height = 0;
+  }
+});
